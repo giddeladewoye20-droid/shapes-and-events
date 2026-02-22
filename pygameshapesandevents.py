4342499f728f6455d8d907e8105588550faddada
giddeladewoye20-droid <giddeladewoye20@gmail.com>
2026-02-22 18:35:15 +0000

    Add files via upload

--- pygameshapesandevents.py
+++ pygameshapesandevents.py
@@ -1,4 +1,6 @@
 import  pygame
+import random
+
 
 
 WIDTH=600
@@ -16,7 +18,7 @@ class Circle():
         self.x=300
         self.y=300
     def draw(self):
-        pygame.draw.circle(screen,self.color,self.radius,(self.x,self.y))
+        pygame.draw.circle(screen,self.color,(self.x,self.y),self.radius,WIDTH)
 
 object=Circle()
 
@@ -27,6 +29,15 @@ while run==True:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             run=False
+        if event.type==pygame.MOUSEBUTTONDOWN:
+            object.radius=object.radius+10
+        if event.type==pygame.MOUSEBUTTONUP:
+            object.color=("yellow")
+            object.x=random.randint(100,300)
+            object.y=random.randint(100,300)
+        if event.type==pygame.MOUSEMOTION:
+            position=pygame.mouse.get_pos()
+            object.x,object.y=position
     
     pygame.display.update()
 
